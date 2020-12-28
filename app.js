2d281b853ce3dda92325af6b286dd3f58fd6dd4d
mooyeon-choi <dus1208@ajou.ac.kr>
2020-12-28 16:01:21 +0900

    Add code from TIL

---
+++ app.js
@@ -0,0 +1,55 @@
+import { Visual } from "./visual.js";
+
+class App {
+  constructor() {
+    this.canvas = document.createElement("canvas");
+    document.body.appendChild(this.canvas);
+
+    this.ctx = this.canvas.getContext("2d");
+
+    this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
+    document.addEventListener("click", this.onClick.bind(this), false);
+
+    WebFont.load({
+      google: {
+        families: ["Hind:700"],
+      },
+      fontactive: () => {
+        this.visual = new Visual();
+
+        window.addEventListener("resize", this.resize.bind(this), false);
+        this.resize();
+
+        requestAnimationFrame(this.animate.bind(this));
+      },
+    });
+  }
+
+  resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
+
+    this.canvas.width = this.stageWidth * this.pixelRatio;
+    this.canvas.height = this.stageHeight * this.pixelRatio;
+    this.ctx.scale(this.pixelRatio, this.pixelRatio);
+
+    this.visual.show(this.stageWidth, this.stageHeight);
+  }
+
+  animate(t) {
+    requestAnimationFrame(this.animate.bind(this));
+
+    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+
+    this.visual.animate(this.ctx, t);
+  }
+
+  onClick() {
+    this.visual.show(this.stageWidth, this.stageHeight);
+  }
+}
+
+window.onload = () => {
+  new App();
+};
